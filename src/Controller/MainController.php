a714dfa2b3ea225e252e6735977b68d6bc287c64
Igor Ochkan <igorp.ochkan@gmail.com>
2020-07-23 12:32:29 +0300

    done template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/main", name="main")
+     * @param ProductRepository $productRepository
+     * @return Response
      */
-    public function index()
+    public function index(ProductRepository $productRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'products' => $productRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/main/date/{date}", name="main")
+     * @param ProductRepository $productRepository
+     * @param Request $request
+     * @return Response
+     */
+    public function getByDate(ProductRepository $productRepository, Request $request): Response
+    {
+        $date = $request->query->get('date');
+        if (isset($date)) {
+
+        }
+
+        return $this->render('main/index.html.twig', [
+            'products' => $products,
         ]);
     }
 }
